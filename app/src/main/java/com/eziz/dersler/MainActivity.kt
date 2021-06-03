d443591d6ea26bf21ef4feba77e03b4967ad4eea
Eziz Rehimov <fantastig@bk.ru>
2021-06-03 17:27:38 +0400

    switchButton

--- app/src/main/java/com/eziz/dersler/MainActivity.kt
+++ app/src/main/java/com/eziz/dersler/MainActivity.kt
@@ -2,11 +2,6 @@ package com.eziz.dersler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,24 +9,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonSee = Button(this)
-        buttonSee.setText(R.string.btnName)
-        buttonSee.layoutParams = LinearLayout.LayoutParams(
-            ViewGroup.LayoutParams.WRAP_CONTENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        )
-        rootContainer?.addView(buttonSee)
-        buttonSee.setOnClickListener {
-            val mesaj = resources.getString(R.string.mesaj)
-            Toast.makeText(this, mesaj, Toast.LENGTH_LONG).show()
-
-            val editText1 = EditText(this)
-            editText1.setText(R.string.text)
-            editText1.layoutParams = LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-            )
-            rootContainer?.addView(editText1)
+        switch1.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked)
+                imageView.setImageResource(R.drawable.turnon)
+            else
+                imageView.setImageResource(R.drawable.turnoff)
         }
 
     }
