c7a97b95cd9410b5fb8dacf3cb0a58f339a5c25d
Eziz Rehimov <fantastig@bk.ru>
2021-06-03 16:42:40 +0400

    textInputLayout(Login design with Toast)

--- app/src/main/java/com/eziz/dersler/MainActivity.kt
+++ app/src/main/java/com/eziz/dersler/MainActivity.kt
@@ -9,19 +9,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (ctv != null) {
-            ctv.isChecked = false
-            ctv.setCheckMarkDrawable(android.R.drawable.checkbox_off_background)
-            ctv.setOnClickListener {
-                ctv.isChecked = !ctv.isChecked
-                ctv.setCheckMarkDrawable(
-                    if (ctv.isChecked) android.R.drawable.checkbox_on_background
-                    else android.R.drawable.checkbox_off_background
-                )
-                val mesaj =
-                    "CheckedTextView :" + getString(if (ctv.isChecked) R.string.secildi else R.string.secilmedi)
-                Toast.makeText(this, mesaj, Toast.LENGTH_LONG).show()
-            }
+
+        button.setOnClickListener {
+            val username = username.text.toString()
+            val password = password.text.toString()
+            Toast.makeText(
+                applicationContext,
+                "Username : $username ,Password: $password",
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 }
