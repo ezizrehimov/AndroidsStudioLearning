c3b2601903f7656f2dcb98ae88359d21457d75ef
Eziz Rehimov <fantastig@bk.ru>
2021-06-03 15:31:20 +0400

    using google icons. xml and png format.

--- app/src/main/java/com/eziz/dersler/MainActivity.kt
+++ app/src/main/java/com/eziz/dersler/MainActivity.kt
@@ -11,15 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var langList = resources.getStringArray(R.array.langs)
-        var adapter =
-            ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, langList)
-        mactv.setAdapter(adapter)
-        mactv.threshold = 1
-        mactv.setTokenizer(MultiAutoCompleteTextView.CommaTokenizer())
-        button.setOnClickListener {
-            val text = "Secilenler :" + mactv.text
-            Toast.makeText(this, text, Toast.LENGTH_LONG).show()
-        }
+
     }
 }
