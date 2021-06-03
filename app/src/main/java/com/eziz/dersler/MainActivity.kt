5e197d526114379a43032684b935b334a526ca73
Eziz Rehimov <fantastig@bk.ru>
2021-06-03 15:40:19 +0400

    checkedTextView

--- app/src/main/java/com/eziz/dersler/MainActivity.kt
+++ app/src/main/java/com/eziz/dersler/MainActivity.kt
@@ -2,8 +2,6 @@ package com.eziz.dersler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.MultiAutoCompleteTextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,6 +9,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        if (ctv != null) {
+            ctv.isChecked = false
+            ctv.setCheckMarkDrawable(android.R.drawable.checkbox_off_background)
+            ctv.setOnClickListener {
+                ctv.isChecked = !ctv.isChecked
+                ctv.setCheckMarkDrawable(
+                    if (ctv.isChecked) android.R.drawable.checkbox_on_background
+                    else android.R.drawable.checkbox_off_background
+                )
+                val mesaj =
+                    "CheckedTextView :" + getString(if (ctv.isChecked) R.string.secildi else R.string.secilmedi)
+                Toast.makeText(this, mesaj, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
